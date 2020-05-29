4bc3e1fb276a47f2e16ff4c884fb7e9e8acd4c3e
David <martirosov.davidka@mail.ru>
2020-05-29 17:44:54 +0300

    2iz8var

--- iz2.pl
+++ iz2.pl
@@ -2,3 +2,32 @@
 % причем если введенное число делится на простое число p в степени a,
 % то в итоговом списке число p должно повторятся a раз.
 % Результирующий список должен быть упорядочен по возрастанию. 
+
+primeDivisors(0, []):-!.
+primeDivisors(1, []):-!.
+primeDivisors(Num, X):- findDivisors(Num, 2, [], X).
+
+
+findDivisors(Num, Num, List, ResultList):- prime(Num),
+                                           append(List, [Num], ResultList), !.
+
+findDivisors(Num, Divisor, List, ResultList):- Remainder is Num mod Divisor,
+                                               Remainder = 0,
+                                               prime(Divisor),
+                                               X is Num / Divisor,
+                                               append(List, [Divisor], NewList),
+                                               findDivisors(X, Divisor, NewList, ResultList), !.
+
+findDivisors(Num, Divisor, List, ResultList):- NextDivisor is Divisor + 1,
+                                               findDivisors(Num, NextDivisor, List, ResultList).
+
+
+% Проверка на простоту.
+prime(X):- prime(X, 2).
+prime(0,_):-!,fail.
+prime(1,_):-!,fail.
+prime(X, N):-N2 is N*N, N2 > X,!.
+prime(X, N):-Remainder is X mod N,
+            Remainder \= 0,
+            N1 is N + 1,
+            prime(X, N1).
