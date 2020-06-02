f8c83a8b1b4b2b3df68eb589aa19f7da969ae07b
David <martirosov.davidka@mail.ru>
2020-06-02 10:46:54 +0300

    2iz8var

--- iz2.pl
+++ iz2.pl
@@ -11,13 +11,16 @@ primeDivisors(Num, X):- findDivisors(Num, 2, [], X).
 findDivisors(Num, Num, List, ResultList):- prime(Num),
                                            append(List, [Num], ResultList), !.
 
+% Остаток = числу mod divisor  -> если поделили без остатка, то проверяем на простоту!
+% Усли проверка прошла, то все ок кидаем в наш список!
 findDivisors(Num, Divisor, List, ResultList):- Remainder is Num mod Divisor,
                                                Remainder = 0,
                                                prime(Divisor),
                                                X is Num / Divisor,
                                                append(List, [Divisor], NewList),
-                                               findDivisors(X, Divisor, NewList, ResultList), !.
+                                               findDivisors(X, Divisor, NewList, ResultList), !. % важная строчка 
 
+% Ну тупо перебор
 findDivisors(Num, Divisor, List, ResultList):- NextDivisor is Divisor + 1,
                                                findDivisors(Num, NextDivisor, List, ResultList).
 
