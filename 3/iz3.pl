6a77f3d3f54f334bfa8a49e2a328b540b1088502
David <martirosov.davidka@mail.ru>
2020-06-08 20:36:35 +0300

    пояснилово

--- 3/iz3.pl
+++ 3/iz3.pl
@@ -3,52 +3,97 @@
 % в соответствии с ростом доли согласных в строках. 
 
 
-% обрабатываем файл, создаем NewPath('new'+path), с помощью tell выбираем вывод, записываем, готово
-rewriteFile(Path):- see(Path), readAllLines(List), seen, atom_concat('New', Path, NewPath), tell(NewPath), writeStringsFromList(List), told.
+
+% обрабатываем файл, создаем NewPath('Result'+path), с помощью tell выбираем вывод, записываем, готово
+rewriteFile(Path):- see(Path), readAllLines(List), seen, atom_concat('Result', Path, NewPath), tell(NewPath), writeStringsFromList(List), told.
+
 
 % получаем строки
-readAllLines(ResultList):- readln(CurStr), readAllLines(CurStr, [], ResultList).
+readAllLines(ResultList):- readln(CurStr), 
+                           readAllLines(CurStr, [], ResultList).
+
 readAllLines([], []):-!.
+
 readAllLines([], CurList, CurList):- !.
-readAllLines(CurStr, CurList, ResultList):- insert(CurList, CurStr, NewList), readln(NewStr), readAllLines(NewStr, NewList, ResultList).
+
+readAllLines(CurStr, CurList, ResultList):- insert(CurList, CurStr, NewList), 
+                                            readln(NewStr), 
+                                            readAllLines(NewStr, NewList, ResultList).
 
 
 % append(?List1, ?List2, ?List1AndList2)
 % List1AndList2 is the concatenation of List1 and List
 insert([], NewStr, [NewStr]):- !.
-insert([H|T], NewStr, ResultList):- numOfConsInStr(NewStr, N1), numOfConsInStr(H, N2), N1 < N2, append([NewStr], [H], TempList), append(TempList, T, ResultList), !.
+
+insert([H|T], NewStr, ResultList):- numOfConsInStr(NewStr, N1), 
+                                    numOfConsInStr(H, N2),
+                                    N1 < N2, 
+                                    append([NewStr], [H], TempList), 
+                                    append(TempList, T, ResultList), 
+                                    !.
+
 insert([H|T], NewStr, ResultList):- insert(T, [H], NewStr, ResultList).
+
 insert([], FirstHalf, NewStr, ResultList):- append(FirstHalf, [NewStr], ResultList), !.
-insert([H|T], FirstHalf, NewStr, ResultList):- numOfConsInStr(NewStr, N1), numOfConsInStr(H, N2), N1 < N2, append(FirstHalf, [NewStr], NewFirstHalf), append(NewFirstHalf, [H], TempHead), append(TempHead, T, ResultList), !.
-insert([H|T], FirstHalf, NewStr, ResultList):- append(FirstHalf, [H], NewFirstHalf), insert(T, NewFirstHalf, NewStr, ResultList).
+
+insert([H|T], FirstHalf, NewStr, ResultList):- numOfConsInStr(NewStr, N1), 
+                                               numOfConsInStr(H, N2), N1 < N2, 
+                                               append(FirstHalf, [NewStr], NewFirstHalf), 
+                                               append(NewFirstHalf, [H], TempHead), 
+                                               append(TempHead, T, ResultList), 
+                                               !.
+
+insert([H|T], FirstHalf, NewStr, ResultList):- append(FirstHalf, [H], NewFirstHalf), 
+                                               insert(T, NewFirstHalf, NewStr, ResultList).
 
 
-% считает согласные в строке
+
+% рекурсивно считает согласные в строке. опустошим список - закончим!
 numOfConsInStr(Str, N):- numOfConsInStr(Str, 0, N).
-numOfConsInStr([], CurN, CurN):- !.
-numOfConsInStr([H|T], CurN, N):- numOfConsInWord(H, N1), NewN is CurN + N1, numOfConsInStr(T, NewN, N).
 
-% считает согласные в слове
+numOfConsInStr([], CurN, CurN):- !.
+
+numOfConsInStr([H|T], CurN, N):- numOfConsInWord(H, N1),
+                                 NewN is CurN + N1, 
+                                 numOfConsInStr(T, NewN, N).
+
+
 % ?- atom_chars(hello, X).
 % X = [h, e, l, l, o]
-numOfConsInWord(Str, N):- atom_chars(Str, Chars), numOfConsInWord(Chars, 0, N).
+% считаем согласные в слове. бьем слово на список из его букв. потом обрабатываем список,
+% проверяя каждую букву гласная ли она. ну и если гласная, то, соответственно, увеличиваем счетчик!
+numOfConsInWord(Str, N):- atom_chars(Str, Chars), 
+                          numOfConsInWord(Chars, 0, N).
+
 numOfConsInWord([], CurN, CurN):- !.
-numOfConsInWord([H|T], CurN, N):- consonant(H), NewN is CurN + 1, numOfConsInWord(T, NewN, N), !.
+
+numOfConsInWord([H|T], CurN, N):- consonant(H),
+                                  NewN is CurN + 1,
+                                  numOfConsInWord(T, NewN, N), 
+                                  !.
+
 numOfConsInWord([_|T], CurN, N):- numOfConsInWord(T, CurN, N).
 
+
 % write(+Term)
-% Write Term to the current output, using brackets and operators where appropriate.
+% Write Term to the current output!
 writeStringsFromList([]):- !.
 writeStringsFromList([H|T]):- write_str(H), write('\n'), writeStringsFromList(T).
+
 
 % put(+Char)
 % Write Char to the current output stream. Char is either an integer expression evaluating to a character code or an atom of one character.
 write_str([]):-!.
 write_str([H|T]):- atom_chars(H, Chars), write_word(Chars), write(' '), write_str(T).
 
+
+% put(+Char).
+% Write Char to the current output stream.
+% рекурсивено записываем слова в текущий выходной поток!
 write_word([]):- !.
 write_word([H|T]):- put(H), write_word(T).
- 
+
+
 consonant('B').
 consonant('C').
 consonant('D').
