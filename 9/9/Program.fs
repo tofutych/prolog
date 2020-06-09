b6e7a12b1528f2d95a167364b3331aecd692bc8b
David <martirosov.davidka@mail.ru>
2020-06-09 03:43:32 +0300

    блин через 4 часа вставать

--- 9/9/Program.fs
+++ 9/9/Program.fs
@@ -4,7 +4,7 @@ let checker num =
     let rec digitsSum num current_num =
         if (num = 0) then current_num
         else 
-            let new_sum = current_num + Math.Pow((float)num % 10.0, 5.0)  //Pow почему то только для float работает 
+            let new_sum = current_num + Math.Pow((float)num % 10.0, 5.0)  // Pow почему-то только для float работает.
             digitsSum (num/10) new_sum  //идем на следуюший шаг рекурсии с отсеченной последней цифрой и новой суммой!
     digitsSum num 0.0
 
@@ -13,10 +13,10 @@ let counter =
         if (current_num = 389140) then sum  (*ограничимся  этим значением - какие-то типы на сайберфоруме оценивали верхнюю границу этой задачи (dxdy.ru/topic104639.html)*)
         else if (checker current_num = (float)current_num) then  
             let new_sum = current_num + sum
-            meow (current_num + 1) new_sum  //если необходимое свойство выполняется, то, естественно суммируем и идем на следующий шаг рекурсии с новым счетчиком и новой суммой!
+            meow (current_num + 1) new_sum  // если необходимое свойство выполняется, то, естественно суммируем и идем на следующий шаг рекурсии с новым счетчиком и новой суммой!
         else
             meow (current_num + 1) sum
-    meow 0 0
+    meow 2 0  // пропускаем единицу, тк 1^5 = 1^5 - не сумма!
             
 
 [<EntryPoint>]
@@ -24,3 +24,4 @@ let main argv =
     let sum = counter
     Console.WriteLine(sum.ToString())
     0
+
