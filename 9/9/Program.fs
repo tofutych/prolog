ffb88f1bac7d894566de87fffd9032ddaca54e3c
David <martirosov.davidka@mail.ru>
2020-06-09 11:23:18 +0300

    meow

--- 9/9/Program.fs
+++ 9/9/Program.fs
@@ -1,12 +1,14 @@
 ﻿open System
 
+
 let checker num = 
     let rec digitsSum num current_num =
         if (num = 0) then current_num
         else 
-            let new_sum = current_num + Math.Pow((float)num % 10.0, 5.0)  // Pow почему-то только для float работает.
-            digitsSum (num/10) new_sum  //идем на следуюший шаг рекурсии с отсеченной последней цифрой и новой суммой!
+            let new_sum = current_num + Math.Pow((float)num % 10.0, 5.0) // Pow почему-то только для float работает.
+            digitsSum (num/10) new_sum  // идем на следуюший шаг рекурсии с отсеченной последней цифрой и новой суммой!
     digitsSum num 0.0
+
 
 let counter = 
     let rec meow current_num sum =
@@ -17,11 +19,10 @@ let counter =
         else
             meow (current_num + 1) sum
     meow 2 0  // пропускаем единицу, тк 1^5 = 1^5 - не сумма!
-            
+
 
 [<EntryPoint>]
 let main argv =
     let sum = counter
     Console.WriteLine(sum.ToString())
     0
-
