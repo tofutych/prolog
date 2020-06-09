cbb574c28faa05b5794a4ad6a5617796d9cda33c
David <martirosov.davidka@mail.ru>
2020-06-09 03:29:21 +0300

    блин через 4,5 часа вставать

--- 9/9/Program.fs
+++ 9/9/Program.fs
@@ -1,8 +1,26 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
 
-open System
+let checker num = 
+    let rec digitsSum num current_num =
+        if (num = 0) then current_num
+        else 
+            let new_sum = current_num + Math.Pow((float)num % 10.0, 5.0)  //Pow почему то только для float работает 
+            digitsSum (num/10) new_sum  //идем на следуюший шаг рекурсии с отсеченной последней цифрой и новой суммой!
+    digitsSum num 0.0
+
+let counter = 
+    let rec meow current_num sum =
+        if (current_num = 389140) then sum  (*ограничимся  этим значением - какие-то типы на сайберфоруме оценивали верхнюю границу этой задачи (dxdy.ru/topic104639.html)*)
+        else if (checker current_num = (float)current_num) then  
+            let new_sum = current_num + sum
+            meow (current_num + 1) new_sum  //если необходимое свойство выполняется, то, естественно суммируем и идем на следующий шаг рекурсии с новым счетчиком и новой суммой!
+        else
+            meow (current_num + 1) sum
+    meow 0 0
+            
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    let sum = counter
+    Console.WriteLine(sum.ToString())
+    0
